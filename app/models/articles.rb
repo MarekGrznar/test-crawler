82c36f6dc1612fc4b74db5c4989ad034fab1eb8d
Stibila <stibila@gmail.com>
2014-07-08 18:43:49 +0200

    Added osel.cz parser

--- app/models/articles.rb
+++ app/models/articles.rb
@@ -1,2 +1,18 @@
 class Articles < ActiveRecord::Base
+
+  def self.parse_from_osel(html)
+    doc = Nokogiri::HTML.parse(html)
+
+    doc.search('.clanek_uvodka_new').map do |div|
+      anchor = div.search('a.nadpis_clanku2').first
+
+      Articles.create!(
+          article_id: anchor[:href][/clanek=(\d+)/, 1].to_i,
+          title: anchor.text,
+          perex: div.search('.popisek').text,
+          url: "http://osel.cz/" + anchor[:href]
+      )
+    end
+  end
+
 end
--- spec/models/articles_spec.rb
+++ spec/models/articles_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe Articles, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'parses from osel.cz' do
+    html = File.read(File.dirname(__FILE__) + '/fixtures/osel_title_page.html', encoding: 'windows-1250')
+    Articles.parse_from_osel(html)
+
+    expect(Articles.count).not_to eq(0)
+
+    article = Articles.first
+
+    expect(article.article_id).to eq(7678)
+    expect(article.title).to eq('Šikovná nanoměřidla z DNA origami pro výzkum rakoviny ')
+    expect(article.perex).to eq('Technologie DNA origami umožňuje poskládat v nanoměřítku takřka cokoliv. Například přesná měřidla pro studium efrinového receptoru EphA2, který je významný v některých typech rakoviny.')
+    expect(article.url).to eq('http://osel.cz/index.php?clanek=7678')
+  end
+
 end
