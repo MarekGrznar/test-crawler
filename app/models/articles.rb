0bb4f3fe25c8c4b2d2341ca4d1ff964e9b9633c4
MarekGrznar <marek.grznar@gmail.com>
2014-07-09 10:33:43 +0200

    Add dsl parser

--- app/models/articles.rb
+++ app/models/articles.rb
@@ -15,4 +15,28 @@ class Articles < ActiveRecord::Base
     end
   end
 
+  def self.parse_from_dsl(html)
+    doc = Nokogiri::HTML.parse(html)
+    first_link = doc.search('a.title_day').first
+
+    Articles.create!(
+      article_id: first_link[:href][/article=(\d+)/, 1].to_i,
+      title: first_link.text,
+      perex: doc.search('.article_perex').first.text,
+      url: "http://dsl.sk/" + first_link[:href]
+    )
+
+    doc.search('td').map do |div|
+      anchor = div.search('> a.title').first
+      perex = div.search('> .article_perex').first
+
+      Articles.create!(
+        article_id: anchor[:href][/article=(\d+)/, 1].to_i,
+        title: anchor.text,
+        perex: perex.text,
+        url: "http://dsl.sk/" + anchor[:href]
+      ) if anchor && perex
+    end
+  end
+
 end
--- spec/models/articles_spec.rb
+++ spec/models/articles_spec.rb
@@ -15,4 +15,26 @@ RSpec.describe Articles, :type => :model do
     expect(article.url).to eq('http://osel.cz/index.php?clanek=7678')
   end
 
+  it 'parses from dsl.sk' do
+    html = File.read(File.dirname(__FILE__) + '/fixtures/dsl-title-page.html', encoding: 'windows-1250')
+    Articles.parse_from_dsl(html)
+
+    expect(Articles.count).not_to eq(0)
+    expect(Articles.count).to eq(13)
+
+    article = Articles.first
+
+    expect(article.article_id).to eq(15760)
+    expect(article.title).to eq('Google ukázal nový Android L, je dvakrát výkonnejší, má dlhšiu výdrž a vyzerá inak')
+    expect(article.perex).to eq('Spoločnosť Google v stredu na svojej konferencii pre vývojárov Google I/O predstavila viacero produktových noviniek vrátane novej verzie svojho operačného systému Android, označovaná spoločnosťou za najvýznamnejšiu novú verziu v celej histórii tohto OS. V novej verzii prišlo k výraznému prepracovaniu dizajnu a rapídne sa má vďaka novému runtimu zvýšiť aj výkon a odozva, lepšia má byť aj spotreba a výdrž.')
+    expect(article.url).to eq('http://dsl.sk/article.php?article=15760&title=')
+
+    second_article = Articles.second
+
+    expect(second_article.article_id).to eq(15698)
+    expect(second_article.title).to eq('FIFA pustila do futbalu technológiu, na MS o góloch rozhoduje 14 kamier s 500 fps')
+    expect(second_article.perex).to eq('Na 20. majstrovstvách sveta vo futbale, ktoré štartujú úvodným zápasom vo štvrtok, bude prvýkrát v histórii asistovať pri rozhodovaní technológia. Po nesprávnych rozhodnutiach rozhodcov okrem iného na predchádzajúcich MS 2010 bude posudzovať padnutie gólu plne automaticky nemecká kamerová technológia GoalControl-4D, ktorá umožní rozhodcom správne rozhodnúť aj v komplikovaných situáciách. Technológia sa spolieha na celkom 14 vysokorýchlostných kamier na jednom štadióne.')
+    expect(second_article.url).to eq('http://dsl.sk/article.php?article=15698&title=')
+  end
+
 end
